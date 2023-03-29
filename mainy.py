acb90fcd3bdf788be7a09fced3808f0ca10305d6
whd3301 <willian.dalon@escola.pr.gov.br>
2023-03-29 20:04:51 -0300

    aula 29/03

---
+++ mainy.py
@@ -0,0 +1,6 @@
+x = float(input("diga um numero"))
+
+result = x ** 2 + 2 * x-4
+
+print(result)
+
---
+++ x1.py
@@ -0,0 +1,6 @@
+raio = float(input("informe o raio do circulo"))
+
+area = 3.14159 * (raio ** 2)
+
+print("a area do circulo e ", area)
+
