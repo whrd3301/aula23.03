44c588ce74aafe2a4b699545984ed07d489c7353
whd3301 <willian.dalon@escola.pr.gov.br>
2023-03-29 21:12:12 -0300

    aula5

---
+++ ex5.py
@@ -0,0 +1,7 @@
+area = float(input("informe a are da casa :´"))
+qtderejunte = area /3
+qtdeargamassa = area /5
+
+msgrejunte =("a quantidade de rejunte necessária é" + str(qtderejunte) +"KG")
+print(msgrejunte)
+print("a quantidade de argamassa necessaria é",qtdeargamassa)
